0f599991f647063ce2779da934627c34db0a8628
tea-aroma <kovalyovictor71@gmail.com>
2025-05-15 01:15:11 +0500

    feat: Add base class for table structures.

---
+++ src/TableStructures/Abstracts/LexiconTableStructure.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace TeaAroma\Lexicon\TableStructures\Abstracts;
+
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use TeaAroma\Lexicon\Enums\LexiconConfig;
+
+
+/**
+ * Provides the base logic for managing table structures.
+ */
+readonly abstract class LexiconTableStructure
+{
+    /**
+     * The table name.
+     *
+     * @var string
+     */
+    public string $tableName;
+
+    /**
+     * @param string $tableName
+     */
+    public function __construct(string $tableName)
+    {
+        $this->tableName = LexiconConfig::DATABASE_PREFIX->getConfig() . $tableName;
+    }
+
+    /**
+     * Creates the table using the structure defined in 'applyProcessing' method.
+     *
+     * @return void
+     */
+    public function create(): void
+    {
+        Schema::create($this->tableName, $this->applyProcessing( ... ));
+    }
+
+    /**
+     * Drops the table if it exits.
+     *
+     * @return void
+     */
+    public function dropIfExist(): void
+    {
+        Schema::dropIfExists($this->tableName);
+    }
+
+    /**
+     * Applies the structure to the table schema.
+     *
+     * @param Blueprint $table
+     *
+     * @return void
+     */
+    abstract public function applyProcessing(Blueprint $table): void;
+}
