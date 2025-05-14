9c45346d60045eee3adfc54fbe26823e2f59230b
tea-aroma <kovalyovictor71@gmail.com>
2025-05-15 01:04:38 +0500

    feat: Add new 'LexiconErrors' enum.

---
+++ src/Enums/LexiconErrors.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace TeaAroma\Lexicon\Enums;
+
+
+/**
+ * The enum contains all possible messages of errors.
+ */
+enum LexiconErrors: string
+{
+    case INVALID_OPTIONS_TYPE = 'The given option class \'%s\' is not a subclass of \'%s\'.';
+
+    case UNDEFINED_CONTEXT = 'The \'%s\' context is not defined.';
+
+    case UNDEFINED_LANGUAGE = 'The \'%s\' language is not defined.';
+
+    /**
+     * Formats the message of error.
+     *
+     * @param string ...$args
+     *
+     * @return string
+     */
+    public function format(string ...$args): string
+    {
+        return sprintf($this->value, ...$args);
+    }
+}
